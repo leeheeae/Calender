fbc0fcd0cbf28155d71e514def79523ef3ec1a1e
leeheeae <chromeee@daum.net>
2021-12-13 18:09:41 +0900

    js

---
+++ js/calender.js
@@ -0,0 +1,8 @@
+let today = new Date();
+let Month = today.getMonth() + 1;
+let theWeek = today.getDay();
+let year = today.getYear();
+let date = today.getDate();
+
+
+console.log(today.getDate());
